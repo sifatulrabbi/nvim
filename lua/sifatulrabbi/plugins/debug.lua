7d3d927fb7c60d462d4869e4fdcbe537b8c263c0
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2023-12-07 17:22:47 +0600

    feat: setup debbuger

--- lua/sifatulrabbi/plugins/debug.lua
+++ lua/sifatulrabbi/plugins/debug.lua
@@ -7,42 +7,29 @@
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 
 return {
-    -- NOTE: Yes, you can install new plugins here!
     'mfussenegger/nvim-dap',
-    -- NOTE: And you can specify dependencies as well
     dependencies = {
-        -- Creates a beautiful debugger UI
         'rcarriga/nvim-dap-ui',
-
-        -- Installs the debug adapters for you
         'williamboman/mason.nvim',
         'jay-babu/mason-nvim-dap.nvim',
-
-        -- Add your own debuggers here
         'leoluz/nvim-dap-go',
+        'theHamsta/nvim-dap-virtual-text',
     },
     config = function()
         local dap = require 'dap'
         local dapui = require 'dapui'
 
+        require('nvim-dap-virtual-text').setup()
+
         require('mason-nvim-dap').setup {
-            -- Makes a best effort to setup the various debuggers with
-            -- reasonable debug configurations
+            automatic_installation = true,
             automatic_setup = true,
-
-            -- You can provide additional configuration to the handlers,
-            -- see mason-nvim-dap README for more information
             handlers = {},
-
-            -- You'll need to check that you have the required things installed
-            -- online, please don't ask me how to install them :)
             ensure_installed = {
-                -- Update this to ensure that you have the debuggers for the langs you want
                 'delve',
             },
         }
 
-        -- Basic debugging keymaps, feel free to change to your liking!
         vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
         vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
         vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
@@ -81,7 +68,6 @@ return {
         dap.listeners.before.event_terminated['dapui_config'] = dapui.close
         dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
-        -- Install golang specific config
         require('dap-go').setup()
     end,
 }
