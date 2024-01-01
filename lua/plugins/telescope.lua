433a291b8816d1ca4a0bb6aca5b0d1ebaa337755
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-01-01 18:49:44 +0600

    change the keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,24 @@ return {
         opts = {},
         config = function()
             -- See `:help telescope` and `:help telescope.setup()`
-            -- require("telescope").setup({})
+            require("telescope").setup({
+                defaults = {
+                    layout_config = {
+                        horizontal = {
+                            preview_width = 0.5,
+                            results_width = 0.5,
+                        },
+                    },
+                },
+                pickers = {
+                    find_files = {
+                        find_command = { "fd", "-H", "-t", "f" },
+                    },
+                    current_buffer_fuzzy_find = {
+                        theme = "dropdown",
+                    },
+                },
+            })
 
             -- Enable telescope fzf native, if installed
             pcall(require("telescope").load_extension, "fzf")
@@ -25,7 +42,7 @@ return {
             -- searching files, words, tags
             vim.keymap.set("n", "<leader>fg", ts_builtin.live_grep, { desc = "Live [g]rep" })
             vim.keymap.set("n", "<leader>ff", ts_builtin.find_files, { desc = "Search [f]iles" })
-            vim.keymap.set("n", "<leader>fb", ts_builtin.current_buffer_fuzzy_find , { desc = "Fuzzily search in current buffer" })
+            vim.keymap.set("n", "<leader>/", ts_builtin.current_buffer_fuzzy_find , { desc = "Fuzzily search in current buffer" })
             vim.keymap.set("n", "<leader>fh", ts_builtin.help_tags, { desc = "Search [h]elp" })
 
             -- helpful telescope features
