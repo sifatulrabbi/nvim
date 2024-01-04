a38ef0441c7fbc8d71297059b1a44d0b90ff01cc
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-01-04 11:43:47 +0600

    colorscheme update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,28 +6,28 @@ return {
             require("catppuccin").setup({
                 show_end_of_buffer = true,
                 no_bold = true,
-                transparent_background = false,
+                transparent_background = true,
             })
 
             vim.cmd.colorscheme("catppuccin")
         end,
     },
 
-    {
-        "rose-pine/neovim",
-        as = "rose-pine",
-        opts = {},
-        config = function()
-            require("rose-pine").setup({
-                variant = "main",
-                dark_variant = "main",
-                dim_nc_background = false,
-                disable_background = false,
-                disable_float_background = false,
-                disable_italics = false,
-            })
-
-            -- vim.cmd.colorscheme("rose-pine")
-        end,
-    },
+    -- {
+    --     "rose-pine/neovim",
+    --     as = "rose-pine",
+    --     opts = {},
+    --     config = function()
+    --         require("rose-pine").setup({
+    --             variant = "main",
+    --             dark_variant = "main",
+    --             dim_nc_background = true,
+    --             disable_background = true,
+    --             disable_float_background = true,
+    --             disable_italics = false,
+    --         })
+    --
+    --         -- vim.cmd.colorscheme("rose-pine")
+    --     end,
+    -- },
 }
