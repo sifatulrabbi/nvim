4eae6f416665fcc76db10326bce82f0e908a29f4
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-02-05 07:29:48 +0600

    update the plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,7 +26,7 @@ return {
                 },
                 pickers = {
                     find_files = {
-                        find_command = { "fd", "-H", "-t", "f" },
+                        find_command = { "fd", "-H", "-t", "f", "--exclude", ".git", "--exclude", "node_modules", "--exclude", ".venv" },
                     },
                     current_buffer_fuzzy_find = {
                         theme = "dropdown",
@@ -42,7 +42,8 @@ return {
             -- searching files, words, tags
             vim.keymap.set("n", "<leader>fg", ts_builtin.live_grep, { desc = "Live [g]rep" })
             vim.keymap.set("n", "<leader>ff", ts_builtin.find_files, { desc = "Search [f]iles" })
-            vim.keymap.set("n", "<leader>/", ts_builtin.current_buffer_fuzzy_find , { desc = "Fuzzily search in current buffer" })
+            vim.keymap.set("n", "<leader>/", ts_builtin.current_buffer_fuzzy_find,
+                { desc = "Fuzzily search in current buffer" })
             vim.keymap.set("n", "<leader>fh", ts_builtin.help_tags, { desc = "Search [h]elp" })
 
             -- helpful telescope features
@@ -56,7 +57,8 @@ return {
             vim.keymap.set("n", "gi", ts_builtin.lsp_implementations, { desc = "LSP: Goto [I]mplementation" })
             vim.keymap.set("n", "gD", ts_builtin.lsp_type_definitions, { desc = "LSP: Type [D]efinition" })
             vim.keymap.set("n", "<leader>fs", ts_builtin.lsp_document_symbols, { desc = "LSP: Document [S]ymbols" })
-            vim.keymap.set("n", "<leader>fS", ts_builtin.lsp_dynamic_workspace_symbols, { desc = "LSP: Workspace [S]ymbols" })
+            vim.keymap.set("n", "<leader>fS", ts_builtin.lsp_dynamic_workspace_symbols,
+                { desc = "LSP: Workspace [S]ymbols" })
         end,
     },
 }
