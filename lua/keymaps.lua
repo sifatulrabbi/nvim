d92fa005ef7026db4cf3359a145f450672bd8d68
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-02-24 16:17:38 +0600

    update keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,14 +2,14 @@
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set( "n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set( "n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set( "n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set( "n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-vim.keymap.set( "n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set( "n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- Movings a selection
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move HL line up" })
@@ -22,9 +22,11 @@ vim.keymap.set("n", "<leader>x", ":Ex<CR>", { desc = "Open Netrw", noremap = tru
 
 -- system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Add to clipboard" })
-vim.keymap.set({"n","v"}, "<leader>p", '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from clipboard" })
 
 vim.keymap.set("n", "<leader>l", "<cmd>Lazy<CR>", { desc = "Open [l]azy" })
 vim.keymap.set("n", "<leader>m", "<cmd>Mason<CR>", { desc = "Open [m]ason" })
 
 vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Save current buffer" })
+
+vim.keymap.set("n", "<C-w>t", ":tabnew<CR>", { desc = "Create new empty tab" })
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -29,11 +29,16 @@ return {
             vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end, { desc = "Add to harpoon list" })
             vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-            vim.keymap.set("n", "<C-y>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-m>", function() harpoon:list():select(4) end)
-            vim.keymap.set("n", "<C-k>", function() harpoon:list():select(5) end)
+            vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<A-5>", function() harpoon:list():select(5) end)
+            vim.keymap.set("n", "<A-6>", function() harpoon:list():select(6) end)
+            vim.keymap.set("n", "<A-7>", function() harpoon:list():select(7) end)
+            vim.keymap.set("n", "<A-8>", function() harpoon:list():select(8) end)
+            vim.keymap.set("n", "<A-9>", function() harpoon:list():select(9) end)
+            vim.keymap.set("n", "<A-0>", function() harpoon:list():select(0) end)
         end,
     },
 }
