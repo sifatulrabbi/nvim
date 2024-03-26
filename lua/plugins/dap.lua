b23b2f756fbb6fc2a15a9a2391e8d5b4257f6c87
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-03-26 09:21:43 +0600

    update configs and versions

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -7,6 +7,7 @@ return {
             "jay-babu/mason-nvim-dap.nvim",
             "leoluz/nvim-dap-go",
             "theHamsta/nvim-dap-virtual-text",
+            "nvim-neotest/nvim-nio",
         },
         config = function()
             local dap = require("dap")
@@ -54,10 +55,11 @@ return {
             vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
             vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
             vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
-            vim.keymap.set( "n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-            vim.keymap.set("n", "<leader>B", function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, { desc = "Debug: Set Breakpoint" })
+            vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+            vim.keymap.set("n", "<leader>B", function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end,
+                { desc = "Debug: Set Breakpoint" })
             -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-            vim.keymap.set( "n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
+            vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
         end,
     },
 }
