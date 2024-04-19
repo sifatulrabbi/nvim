243025bd25f858f905fd87a4c264ce986add60d4
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-04-19 08:27:25 +0600

    oil nvim

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -38,3 +38,4 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -19,6 +19,52 @@ return {
     },
 
     {
+        "stevearc/oil.nvim",
+        opts = {
+            default_file_explorer = true,
+            delete_to_trash = true,
+            view_options = {
+                show_hidden = true,
+                sort = {
+                    -- sort order can be "asc" or "desc"
+                    -- see :help oil-columns to see which columns are sortable
+                    { "type", "asc" },
+                    { "name", "asc" },
+                },
+            },
+            columns = {
+                "icon",
+                "permissions",
+                "size",
+                -- "mtime",
+            },
+            lsp_file_methods = {
+                timeout_ms = 1000,
+                autosave_changes = false,
+            },
+            keymaps = {
+                ["g?"] = "actions.show_help",
+                ["<CR>"] = "actions.select",
+                ["<C-s>"] = "actions.select_vsplit",
+                ["<C-h>"] = "actions.select_split",
+                ["<C-t>"] = "actions.select_tab",
+                ["<C-p>"] = "actions.preview",
+                ["<C-c>"] = "actions.close",
+                ["<C-l>"] = "actions.refresh",
+                ["-"] = "actions.parent",
+                ["_"] = "actions.open_cwd",
+                ["`"] = "actions.cd",
+                ["~"] = "actions.tcd",
+                ["gs"] = "actions.change_sort",
+                ["gx"] = "actions.open_external",
+                ["g."] = "actions.toggle_hidden",
+                ["g\\"] = "actions.toggle_trash",
+            },
+        },
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+
+    {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
         opts = {},
