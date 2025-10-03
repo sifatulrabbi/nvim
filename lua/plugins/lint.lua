32a7daf390956277e8ce8d50f2e8067d5ac738b2
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2025-10-03 15:48:12 +0600

    updating base configs

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,75 +1,52 @@
 return {
-	{
-		"mfussenegger/nvim-lint",
-		event = "VeryLazy",
-		config = function()
-			local lint = require("lint")
+    {
+        "mfussenegger/nvim-lint",
+        event = "VeryLazy",
+        config = function()
+            local lint = require("lint")
 
-			lint.linters_by_ft = {
-				go = { "golangcilint" },
-				-- python = { "flake8" },
-				vue = { "volar" },
-				-- typescriptreact = { "eslint_d" },
-				-- javascriptreact = { "eslint_d" },
-				-- javascript = { "eslint_d" },
-				-- typescript = { "eslint_d" },
-				html = { "htmlhint" },
-				css = { "stylelint" },
-				scss = { "stylelint" },
-				json = { "jsonlint" },
-				jsonc = { "jsonlint" },
-				dockerfile = { "hadolint" },
-			}
+            lint.linters_by_ft = {
+                go = { "golangcilint" },
+                python = { "flake8" },
+                vue = { "volar" },
+                typescriptreact = { "eslint_d" },
+                javascriptreact = { "eslint_d" },
+                javascript = { "eslint_d" },
+                typescript = { "eslint_d" },
+                html = { "htmlhint" },
+                css = { "stylelint" },
+                scss = { "stylelint" },
+                json = { "jsonlint" },
+                jsonc = { "jsonlint" },
+                dockerfile = { "hadolint" },
+            }
 
-			local timer = assert(vim.uv.new_timer())
-			local DEBOUNCE_MS = 500
-			local aug = vim.api.nvim_create_augroup("Lint", { clear = true })
-			local spell_auggrp = vim.api.nvim_create_augroup("LintSpelling", { clear = true })
+            local timer = assert(vim.uv.new_timer())
+            local DEBOUNCE_MS = 500
+            local aug = vim.api.nvim_create_augroup("Lint", { clear = true })
 
-			vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
-				group = aug,
-				callback = function()
-					local bufnr = vim.api.nvim_get_current_buf()
-					timer:stop()
-					timer:start(
-						DEBOUNCE_MS,
-						0,
-						vim.schedule_wrap(function()
-							if vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].modifiable then
-								vim.api.nvim_buf_call(bufnr, function()
-									vim.diagnostic.reset(lint.get_namespace(), bufnr)
-									local ok, _ = pcall(lint.try_lint)
-									if not ok then
-										vim.notify("Linting failed", vim.log.levels.WARN)
-									end
-								end)
-							end
-						end)
-					)
-				end,
-			})
-
-			vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
-				group = spell_auggrp,
-				callback = function()
-					local bufnr = vim.api.nvim_get_current_buf()
-					timer:stop()
-					timer:start(
-						DEBOUNCE_MS / 2,
-						0,
-						vim.schedule_wrap(function()
-							if vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].modifiable then
-								vim.api.nvim_buf_call(bufnr, function()
-									local ok, _ = pcall(lint.try_lint, "cspell")
-									if not ok then
-										-- Silently fail for spell check
-									end
-								end)
-							end
-						end)
-					)
-				end,
-			})
-		end,
-	},
+            vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+                group = aug,
+                callback = function()
+                    local bufnr = vim.api.nvim_get_current_buf()
+                    timer:stop()
+                    timer:start(
+                        DEBOUNCE_MS,
+                        0,
+                        vim.schedule_wrap(function()
+                            if vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].modifiable then
+                                vim.api.nvim_buf_call(bufnr, function()
+                                    vim.diagnostic.reset(lint.get_namespace(), bufnr)
+                                    local ok, _ = pcall(lint.try_lint)
+                                    if not ok then
+                                        vim.notify("Linting failed", vim.log.levels.WARN)
+                                    end
+                                end)
+                            end
+                        end)
+                    )
+                end,
+            })
+        end,
+    },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,188 +1,188 @@
 return {
-	{ "MunifTanjim/nui.nvim", lazy = true },
+    { "MunifTanjim/nui.nvim",         lazy = true },
 
-	{
-		"nvim-lualine/lualine.nvim",
-		event = "VeryLazy",
-		opts = {
-			options = {
-				icons_enabled = true,
-				theme = "gruvbox",
-				component_separators = "|",
-				section_separators = " ",
-				globalstatus = true,
-			},
-			sections = {
-				lualine_c = {
-					{ "filename", path = 1 },
-				},
-			},
-		},
-	},
+    {
+        "nvim-lualine/lualine.nvim",
+        event = "VeryLazy",
+        opts = {
+            options = {
+                icons_enabled = true,
+                theme = "gruvbox",
+                component_separators = "|",
+                section_separators = " ",
+                globalstatus = true,
+            },
+            sections = {
+                lualine_c = {
+                    { "filename", path = 1 },
+                },
+            },
+        },
+    },
 
-	{
-		"stevearc/oil.nvim",
-		opts = {
-			default_file_explorer = true,
-			delete_to_trash = true,
-			view_options = {
-				show_hidden = true,
-				sort = {
-					-- sort order can be "asc" or "desc"
-					-- see :help oil-columns to see which columns are sortable
-					{ "type", "asc" },
-					{ "name", "asc" },
-				},
-				is_always_hidden = function(name, bufnr)
-					return name == ".git"
-				end,
-			},
-			columns = {
-				"icon",
-				"permissions",
-				"size",
-				"mtime",
-			},
-			lsp_file_methods = {
-				timeout_ms = 1000,
-				autosave_changes = false,
-			},
-			keymaps = {
-				["g?"] = "actions.show_help",
-				["<CR>"] = "actions.select",
-				["<C-S>"] = "actions.select_vsplit",
-				["<C-h>"] = "actions.select_split",
-				["<C-t>"] = "actions.select_tab",
-				["<C-p>"] = "actions.preview",
-				["<C-c>"] = "actions.close",
-				["<C-l>"] = "actions.refresh",
-				["-"] = "actions.parent",
-				["_"] = "actions.open_cwd",
-				["`"] = "actions.cd",
-				["~"] = "actions.tcd",
-				["gs"] = "actions.change_sort",
-				["gx"] = "actions.open_external",
-				["g."] = "actions.toggle_hidden",
-				["g\\"] = "actions.toggle_trash",
-			},
-		},
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
+    {
+        "stevearc/oil.nvim",
+        opts = {
+            default_file_explorer = true,
+            delete_to_trash = true,
+            view_options = {
+                show_hidden = true,
+                sort = {
+                    -- sort order can be "asc" or "desc"
+                    -- see :help oil-columns to see which columns are sortable
+                    { "type", "asc" },
+                    { "name", "asc" },
+                },
+                is_always_hidden = function(name, bufnr)
+                    return name == ".git"
+                end,
+            },
+            columns = {
+                "icon",
+                "permissions",
+                "size",
+                "mtime",
+            },
+            lsp_file_methods = {
+                timeout_ms = 1000,
+                autosave_changes = false,
+            },
+            keymaps = {
+                ["g?"] = "actions.show_help",
+                ["<CR>"] = "actions.select",
+                ["<C-S>"] = "actions.select_vsplit",
+                ["<C-h>"] = "actions.select_split",
+                ["<C-t>"] = "actions.select_tab",
+                ["<C-p>"] = "actions.preview",
+                ["<C-c>"] = "actions.close",
+                ["<C-l>"] = "actions.refresh",
+                ["-"] = "actions.parent",
+                ["_"] = "actions.open_cwd",
+                ["`"] = "actions.cd",
+                ["~"] = "actions.tcd",
+                ["gs"] = "actions.change_sort",
+                ["gx"] = "actions.open_external",
+                ["g."] = "actions.toggle_hidden",
+                ["g\\"] = "actions.toggle_trash",
+            },
+        },
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
 
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = {},
-		config = function()
-			local highlight = {
-				"RainbowRed",
-				"RainbowYellow",
-				"RainbowBlue",
-				"RainbowOrange",
-				"RainbowGreen",
-				"RainbowViolet",
-				"RainbowCyan",
-			}
-			local hooks = require("ibl.hooks")
-			-- create the highlight groups in the highlight setup hook, so they are reset
-			-- every time the colorscheme changes
-			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-				vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-				vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-				vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-				vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-				vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-				vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-				vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-			end)
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = {},
+        config = function()
+            local highlight = {
+                "RainbowRed",
+                "RainbowYellow",
+                "RainbowBlue",
+                "RainbowOrange",
+                "RainbowGreen",
+                "RainbowViolet",
+                "RainbowCyan",
+            }
+            local hooks = require("ibl.hooks")
+            -- create the highlight groups in the highlight setup hook, so they are reset
+            -- every time the colorscheme changes
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+                vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+                vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+                vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+                vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+                vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+                vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+            end)
 
-			vim.g.rainbow_delimiters = { highlight = highlight }
-			require("ibl").setup({ scope = { highlight = highlight } })
-			hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-		end,
-	},
+            vim.g.rainbow_delimiters = { highlight = highlight }
+            require("ibl").setup({ scope = { highlight = highlight } })
+            hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+        end,
+    },
 
-	{
-		"folke/which-key.nvim",
-		opts = {},
-		-- config = function()
-		--     require("which-key").register({
-		--         ["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
-		--     })
-		-- end,
-	},
+    {
+        "folke/which-key.nvim",
+        opts = {},
+        -- config = function()
+        --     require("which-key").register({
+        --         ["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
+        --     })
+        -- end,
+    },
 
-	{
-		"lewis6991/gitsigns.nvim",
-		name = "gitsigns",
-		config = function()
-			require("gitsigns").setup({
-				signs = {
-					add = { text = "+" },
-					change = { text = "~" },
-					delete = { text = "-" },
-					topdelete = { text = "^" },
-					changedelete = { text = "~" },
-					untracked = { text = "?" },
-				},
-				on_attach = function(bufnr)
-					vim.keymap.set(
-						"n",
-						"<leader>hp",
-						require("gitsigns").preview_hunk,
-						{ buffer = bufnr, desc = "Preview git hunk" }
-					)
+    {
+        "lewis6991/gitsigns.nvim",
+        name = "gitsigns",
+        config = function()
+            require("gitsigns").setup({
+                signs = {
+                    add = { text = "+" },
+                    change = { text = "~" },
+                    delete = { text = "-" },
+                    topdelete = { text = "^" },
+                    changedelete = { text = "~" },
+                    untracked = { text = "?" },
+                },
+                on_attach = function(bufnr)
+                    vim.keymap.set(
+                        "n",
+                        "<leader>hp",
+                        require("gitsigns").preview_hunk,
+                        { buffer = bufnr, desc = "Preview git hunk" }
+                    )
 
-					-- don't override the built-in and fugitive keymaps
-					local gs = package.loaded.gitsigns
-					vim.keymap.set({ "n", "v" }, "]c", function()
-						if vim.wo.diff then
-							return "]c"
-						end
-						vim.schedule(function()
-							gs.next_hunk()
-						end)
-						return "<Ignore>"
-					end, {
-						expr = true,
-						buffer = bufnr,
-						desc = "Jump to next hunk",
-					})
-					vim.keymap.set({ "n", "v" }, "[c", function()
-						if vim.wo.diff then
-							return "[c"
-						end
-						vim.schedule(function()
-							gs.prev_hunk()
-						end)
-						return "<Ignore>"
-					end, {
-						expr = true,
-						buffer = bufnr,
-						desc = "Jump to previous hunk",
-					})
-				end,
-			})
-		end,
-	},
+                    -- don't override the built-in and fugitive keymaps
+                    local gs = package.loaded.gitsigns
+                    vim.keymap.set({ "n", "v" }, "]c", function()
+                        if vim.wo.diff then
+                            return "]c"
+                        end
+                        vim.schedule(function()
+                            gs.next_hunk()
+                        end)
+                        return "<Ignore>"
+                    end, {
+                        expr = true,
+                        buffer = bufnr,
+                        desc = "Jump to next hunk",
+                    })
+                    vim.keymap.set({ "n", "v" }, "[c", function()
+                        if vim.wo.diff then
+                            return "[c"
+                        end
+                        vim.schedule(function()
+                            gs.prev_hunk()
+                        end)
+                        return "<Ignore>"
+                    end, {
+                        expr = true,
+                        buffer = bufnr,
+                        desc = "Jump to previous hunk",
+                    })
+                end,
+            })
+        end,
+    },
 
-	{ "kyazdani42/nvim-web-devicons", opts = {} },
+    { "kyazdani42/nvim-web-devicons", opts = {} },
 
-	{ "echasnovski/mini.nvim", version = "*" },
+    { "echasnovski/mini.nvim",        version = "*" },
 
-	{
-		"folke/zen-mode.nvim",
-		config = function()
-			require("zen-mode").setup({
-				window = {
-					width = 120,
-					height = 1,
-					options = {
-						wrap = true,
-					},
-				},
-			})
-			vim.api.nvim_set_keymap("n", "<leader>z", "<cmd>ZenMode<CR>", { noremap = true, silent = true })
-		end,
-	},
+    {
+        "folke/zen-mode.nvim",
+        config = function()
+            require("zen-mode").setup({
+                window = {
+                    width = 120,
+                    height = 1,
+                    options = {
+                        wrap = true,
+                    },
+                },
+            })
+            vim.api.nvim_set_keymap("n", "<leader>z", "<cmd>ZenMode<CR>", { noremap = true, silent = true })
+        end,
+    },
 }
