c242a19a7193e33a040a478fdc57bd80c932e299
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2026-01-21 19:29:01 +0600

    remove transparency from ui

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -22,7 +22,7 @@ return {
       palette_overrides = {},
       overrides = {},
       dim_inactive = false,
-      transparent_mode = true,
+      transparent_mode = false,
     },
   },
 
@@ -30,8 +30,8 @@ return {
   --   "rose-pine/neovim",
   --   name = "rose-pine",
   --   opts = {
-  --     variant = "main", -- auto, main, moon, or dawn
-  --     dark_variant = "main", -- main, moon, or dawn
+  --     variant = "auto", -- auto, main, moon, or dawn
+  --     -- dark_variant = "main", -- main, moon, or dawn
   --     dim_inactive_windows = false,
   --     extend_background_behind_borders = true,
   --
