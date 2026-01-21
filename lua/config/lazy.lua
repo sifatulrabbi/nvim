602d0e4a7bcad17ed62625282775eb8fb8867f41
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2026-01-21 19:49:19 +0600

    add rust

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,6 +33,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     -- import/override with your plugins
     { import = "plugins" },
   },
